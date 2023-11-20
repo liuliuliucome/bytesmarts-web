f72ae4c1807e06b5b585dcaa5bd096cb2407d628
qlover <myused@sina.com>
2023-11-20 20:30:42 +0800

    build: local format

---
+++ work/config/workConfig.js
@@ -0,0 +1,37 @@
+const pkg = require("package.json");
+
+const env = (val, defaultV = "") => val || defaultV;
+/**
+ * @type {Array<'zh' | 'en'>}
+ */
+const locales = ["zh", "en"];
+
+const workConfig = {
+  /**
+   * @type string
+   */
+  versoin: pkg.version,
+
+  /**
+   * 当前环境
+   *
+   * @type {'local'| 'test'| 'qa' |'prod'}
+   */
+  appEnv: env(process.env.NEXT_PUBLIC_APP_ENV),
+
+  /**
+   * 站点域名
+   * @type {string}
+   */
+  siteURL: env(process.env.NEXT_PUBLIC_SITE_URL),
+
+  i18n: {
+    locales: locales,
+    /**
+     * @type {typeof locales[number]}
+     */
+    defaultLocale: locales[0],
+  },
+};
+
+module.exports = workConfig;
