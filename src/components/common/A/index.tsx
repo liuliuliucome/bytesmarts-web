57e0a3a1dc57d884c525105c71ebb7f42d070fd6
qlover <myused@sina.com>
2023-12-14 21:55:22 +0800

    fix: LocalesUtil

--- src/components/common/A/index.tsx
+++ src/components/common/A/index.tsx
@@ -20,27 +20,14 @@ export type HrefType = AProps["href"];
  *
  * nextjs13 以下版本 `Link` 组件会存在问题 `https://nextjs.org/docs/messages/link-multiple-children`
  *
+ * TODO: server 和 client 无法内部修改 href，会导致 href不一致，所有暂时 locale 由外部决定
  * @param param0
  * @returns
  */
 export const A = forwardRef<HTMLAnchorElement, Component.WithChildren<AProps>>(
   (props, ref) => {
-    const { href, locale } = props;
-    const { lang } = useApp();
-    const _href = useMemo(() => {
-      const _locale = isString(locale) ? (locale as I18n.Locale) : lang;
-      if (isString(href)) {
-        return LocalesUtil.toHref(href, _locale);
-      }
-
-      if (href.pathname) {
-        href.pathname = LocalesUtil.toHref(href.pathname, _locale);
-      }
-      return href;
-    }, [href, lang, locale]);
-
     return (
-      <Link {...props} href={_href} ref={ref}>
+      <Link {...props} ref={ref}>
         {props.children}
       </Link>
     );
